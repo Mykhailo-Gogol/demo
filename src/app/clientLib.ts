abe225211d643c622807feb137314517d7cf8e5f
Mykhailo Gogol <68640759+Mykhailo-Gogol@users.noreply.github.com>
2023-04-19 22:27:22 +0300

    next 12

--- src/app/clientLib.ts
+++
@@ -1,5 +0,0 @@
-'use client';
-
-import LoginHero from './components/LoginHero';
-
-export default LoginHero;
---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import Card from "../components/Card";
+import Hero from "../components/Hero";
+import LoginHero from "../components/LoginHero";
+
+import I1 from "../static/1.jpg";
+import I2 from "../static/2.jpg";
+import I3 from "../static/3.jpg";
+
+export default function Home() {
+  return (
+    <main className="px-4 lg:px-24">
+      <Hero />
+      <LoginHero title="Login now!" />
+
+      <ul className="grid md:grid-cols-2 lg:grid-cols-3 gap-10">
+        <li>
+          <Card image={I1} />
+        </li>
+        <li>
+          <Card image={I2} />
+        </li>
+        <li>
+          <Card image={I3} />
+        </li>
+      </ul>
+    </main>
+  );
+}
