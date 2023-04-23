91213685659f2139fc44b0fc42b9499718fccc4e
Mykhailo Gogol <68640759+Mykhailo-Gogol@users.noreply.github.com>
2023-04-23 18:04:33 +0300

    refactoring

--- src/pages/text-completion/index.tsx
+++ src/pages/text-completion/index.tsx
@@ -1,21 +1,61 @@
 import { openai } from "@/api/openapi";
-import React from "react";
+import React, { FormEvent, useState } from "react";
 
-export async function getServerSideProps() {
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: "Say this is a test",
-    max_tokens: 7,
-    temperature: 0,
-  });
+export default function App() {
+  const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState<any>(null);
 
-  return {
-    props: {
-      response: response.data,
-    },
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const res = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      max_tokens: 30,
+      temperature: 0,
+    });
+
+    return setResponse(res);
   };
-}
 
-export default function App({ response }: any) {
-  return <div>{JSON.stringify(response)}</div>;
+  const text: string = (response?.data.choices[0].text as string).replaceAll(
+    "\n",
+    ""
+  );
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="mb-20">
+        <div className="form-control w-full max-w-xs mb-5">
+          <label className="label">
+            <span className="label-text">Write a prompt</span>
+          </label>
+          <input
+            type="text"
+            placeholder="What`s on your mind?"
+            className="input input-bordered w-full max-w-xs"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+          />
+        </div>
+        <button
+          type="submit"
+          className="btn btn-ghost"
+          disabled={!Boolean(prompt)}
+        >
+          Submit
+        </button>
+      </form>
+
+      <div>
+        {response ? (
+          <span>{text}</span>
+        ) : (
+          <span className="label-text">
+            What`s on your mind? Max answear length is 30
+          </span>
+        )}
+      </div>
+    </div>
+  );
 }
